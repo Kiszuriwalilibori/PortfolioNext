c86cb3ae02a014062029acbc9342b3b01f7a17db
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-05-23 15:23:09 +0200

    more bugs fixed for deployment

--- pages/app-with-layouts.jsx
+++ pages/app-with-layouts.jsx
@@ -3,11 +3,13 @@ import { AppProps } from "next/app";
 import MainLayout from "../src/components/layout/main-layout";
 
 function AppWithLayouts({ Component, pageProps }) {
-    const renderWithLayout =
-        Component.getLayout ||
-        function (page) {
-            return <MainLayout>{page}</MainLayout>;
-        };
-    return renderWithLayout(<Component {...pageProps} />);
+    if (Component) {
+        const renderWithLayout =
+            Component?.getLayout ||
+            function (page) {
+                return <MainLayout>{page}</MainLayout>;
+            };
+        return renderWithLayout(<Component {...pageProps} />);
+    } else return null;
 }
 export default AppWithLayouts;
