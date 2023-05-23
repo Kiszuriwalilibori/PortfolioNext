ca4c280cb94e9ec3fbc11f869514924c023750be
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-05-23 15:29:17 +0200

    console.logs removed

--- pages/projects/createFeaturesList.ts
+++ pages/projects/createFeaturesList.ts
@@ -2,7 +2,7 @@ import { Project } from "../../types";
 
 export default function createFeaturesList(data: Project[]) {
     const temp: string[] = [];
-    console.log("data from createfeaturelists", data);
+
     if (data && data.length) {
         data.forEach(item => item.features && temp.push(...item.features));
         return Array.from(new Set(temp)).sort(function (a, b) {
