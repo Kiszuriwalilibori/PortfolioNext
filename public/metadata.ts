677b9ba276086116aeb3e77001e060401b937f57
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-03-21 10:51:25 +0100

    test of dynamic metadata for pages

---
+++ public/metadata.ts
@@ -0,0 +1,10 @@
+interface Metadata {
+    title: string;
+    pageName: string;
+}
+
+export const metadata: Metadata[] = [
+    { title: "Basic infromations about author", pageName: "about" },
+    { title: "Career", pageName: "career" },
+    { title: "Skills", pageName: "skills" },
+];
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,13 @@
-export const metadata = {
-    title: "Piotr Maksymiuk/Front-end Developer/Portfolio/About",
-};
+import { headers } from "next/headers";
+export async function generateMetadata() {
+    const headerList = headers();
+    const pathname = (await headerList).get("x-current-path");
 
-export default function About() {
+    return {
+        title: "generated metadata" + pathname,
+    };
+}
+
+export default async function About() {
     return <h1>About me</h1>;
 }
