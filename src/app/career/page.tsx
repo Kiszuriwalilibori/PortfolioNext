4b6272bc286db479e2b7c925ef569093a31754dd
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-04-05 17:30:18 +0200

    temporarily disabled career page

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,21 +1,21 @@
 import { useId } from "react";
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+// import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 
 import "react-vertical-timeline-component/style.min.css";
 
 import Icons from "./icons";
 
-import { Company, Duties, Position, Project } from "./styled";
-import PageTitle from "@/components/pageTitle";
-import { Job } from "@/types";
-import career from "@/data/career";
+// import { Company, Duties, Position, Project } from "./styled";
+// import PageTitle from "@/components/pageTitle";
+// // import { Job } from "@/types";
+// import career from "@/data/career";
 
 function Career() {
     const ID = useId();
 
     return (
         <section className="jobs">
-            <article className="jobs__content" aria-label=" details about author's jobs">
+            {/* <article className="jobs__content" aria-label=" details about author's jobs">
                 <div className="container">
                     <PageTitle title="Jobs" isWhite />
                     <VerticalTimeline layout={"1-column-left"}>
@@ -44,7 +44,7 @@ function Career() {
                         })}
                     </VerticalTimeline>
                 </div>
-            </article>
+            </article> */}
         </section>
     );
 }
