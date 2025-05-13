d40db6175b3618954c68453042a9dc8e39f0c128
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-05-13 11:21:49 +0200

    fixed bug

--- src/hooks/useSubscribeComments.ts
+++ src/hooks/useSubscribeComments.ts
@@ -1,26 +1,27 @@
-import { getFirestore, collection, query, where } from "firebase/firestore";
-import { useCollection } from "react-firebase-hooks/firestore";
-import firebase_app from "fbase/config";
+// import { getFirestore, collection, query, where } from "firebase/firestore";
+// import { useCollection } from "react-firebase-hooks/firestore";
+// import firebase_app from "fbase/config";
 
-function byField(fieldName: string) {
-    return (a: any, b: any) => (a[fieldName] > b[fieldName] ? 1 : -1);
-}
+// function byField(fieldName: string) {
+//     return (a: any, b: any) => (a[fieldName] > b[fieldName] ? 1 : -1);
+// }
 
-export const useSubscribeComments = (ID: string) => {
-    const ref = query(collection(getFirestore(firebase_app), "comments"), where("projectID", "==", ID));
-    const [value, loading, error] = useCollection(ref, {
-        snapshotListenOptions: { includeMetadataChanges: true },
-    });
+// export const useSubscribeComments = (ID: string) => {
+//     const ref = query(collection(getFirestore(firebase_app), "comments"), where("projectID", "==", ID));
+//     const [value, loading, error] = useCollection(ref, {
+//         snapshotListenOptions: { includeMetadataChanges: true },
+//     });
 
-    const unsortedComments = value
-        ? value.docs.map(doc => {
-              return { ...doc.data(), ID: doc.id };
-          })
-        : null;
+//     const unsortedComments = value
+//         ? value.docs.map(doc => {
+//               return { ...doc.data(), ID: doc.id };
+//           })
+//         : null;
 
-    const comments = unsortedComments ? unsortedComments.sort(byField("created")) : undefined;
+//     const comments = unsortedComments ? unsortedComments.sort(byField("created")) : undefined;
 
-    return { comments, loading, error };
-};
+//     return { comments, loading, error };
+// };
 
-export default useSubscribeComments;
+// export default useSubscribeComments;
+export default {};
