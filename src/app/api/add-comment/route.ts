475863b9b9f63a33852bbdc1d2b483153483adb9
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-06-28 23:50:40 +0200

    check for existing comment to separate function

--- src/app/api/add-comment/route.ts
+++ src/app/api/add-comment/route.ts
@@ -1,8 +1,26 @@
-import { addDoc, collection, getFirestore, query, where, getDocs, orderBy, limit } from "firebase/firestore";
+import { addDoc, collection, getFirestore, query, where, getDocs, orderBy, limit, Firestore } from "firebase/firestore";
 import firebase_app from "@/fbase/config";
 import { CommentType } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+
+/**
+ * Checks if the user has posted a comment on the same project within the last minute.
+ * @param db - Firestore database instance
+ * @param comment - The comment object to check
+ * @returns {Promise<boolean>} - Returns true if a recent comment exists, false otherwise
+ */
+const FLOOD_LIMIT = 60000; // 1 minute in milliseconds
+async function hasRecentComment(db: Firestore, comment: CommentType): Promise<boolean> {
+    const oneMinuteAgo = Date.now() - FLOOD_LIMIT;
+    const userCommentsQuery = query(collection(db, "comments"), where("projectID", "==", comment.projectID), where("authorEmail", "==", comment.authorEmail));
+    const userCommentsSnapshot = await getDocs(userCommentsQuery);
+
+    return userCommentsSnapshot.docs.some(doc => {
+        const commentData = doc.data();
+        return commentData.created > oneMinuteAgo;
+    });
+}
 
 export async function POST(request: NextRequest) {
     try {
@@ -13,17 +31,8 @@ export async function POST(request: NextRequest) {
         }
 
         const db = getFirestore(firebase_app);
-        const oneMinuteAgo = Date.now() - 60000;
-        const userCommentsQuery = query(collection(db, "comments"), where("projectID", "==", comment.projectID), where("authorEmail", "==", comment.authorEmail));
 
-        const userCommentsSnapshot = await getDocs(userCommentsQuery);
-
-        const hasRecentComment = userCommentsSnapshot.docs.some(doc => {
-            const commentData = doc.data();
-            return commentData.created > oneMinuteAgo;
-        });
-
-        if (hasRecentComment) {
+        if (await hasRecentComment(db, comment)) {
             return NextResponse.json({ error: "Please wait at least 1 minute before posting another comment on this project" }, { status: 429 });
         }
 
