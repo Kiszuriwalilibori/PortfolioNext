bfbd4947a16292d4f90aae74e0d124b38273e38e
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-07-22 13:39:24 +0200

    fixed image sizing in mobile view

--- src/components/sidebar/mobileSidebar/index.tsx
+++ src/components/sidebar/mobileSidebar/index.tsx
@@ -5,7 +5,15 @@ import Contacts from "./contacts";
 export function MobileSidebar() {
     return (
         <article className="header-mobile" id="Mobile Sidebar">
-            <Image width={100} height={100} className="image" src="/images/author.jpg" alt="author image" />
+            <Image
+    width={100}
+    height={100}
+    className="image"
+    src="/images/author.webp"
+    alt="author image"
+    sizes="(max-width: 600px) 100vw, 100px"
+    priority
+/>
             <Typography variant="sidebarName">Piotr Maksymiuk</Typography>
             <h2 className="description">Front-End Developer</h2>
             <Contacts />
