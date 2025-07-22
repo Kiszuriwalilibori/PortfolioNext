f0eff8271037d1dfeb192a64733bb41834c9a1b0
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-07-22 13:49:57 +0200

    another fix of author-image size

--- src/components/sidebar/mobileSidebar/index.tsx
+++ src/components/sidebar/mobileSidebar/index.tsx
@@ -6,12 +6,12 @@ export function MobileSidebar() {
     return (
         <article className="header-mobile" id="Mobile Sidebar">
             <Image
-    width={100}
-    height={100}
+    width={150}
+    height={150}
     className="image"
     src="/images/author.webp"
     alt="author image"
-    sizes="(max-width: 600px) 100vw, 100px"
+    sizes="(max-width: 767px) 130px, (max-width: 991px) 140px, 150px"
     priority
 />
             <Typography variant="sidebarName">Piotr Maksymiuk</Typography>
