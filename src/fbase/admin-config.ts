e3afa3063b442fb5ca98b688e2308b9c9693a133
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-10-03 11:14:41 +0200

    Zmiany w kluczach w admin config, prechodzą na formułę bez nextpublic firebase private key i firebase private email. Jednocześnie wrzuciłem obie zmiany na Vercela, jednakże tam nie usuwałem starych wersji póki co

--- src/fbase/admin-config.ts
+++ src/fbase/admin-config.ts
@@ -1,16 +1,38 @@
+// import { initializeApp, getApps, getApp, cert } from "firebase-admin/app";
+
+// const serviceAccount = {
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+//     clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+// };
+
+// export const getFirebaseAdminApp = () => {
+//     if (getApps().length === 0) {
+//         return initializeApp({
+//             credential: cert(serviceAccount),
+//         });
+//     }
+//     return getApp();
+// };
 import { initializeApp, getApps, getApp, cert } from "firebase-admin/app";
 
-const serviceAccount = {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-};
-
-export const getFirebaseAdminApp = () => {
+const getFirebaseAdminApp = () => {
     if (getApps().length === 0) {
+        if (!process.env.FIREBASE_PRIVATE_KEY) {
+            throw new Error('Missing FIREBASE_PRIVATE_KEY environment variable');
+        }
+
+        const serviceAccount = {
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        };
+
         return initializeApp({
             credential: cert(serviceAccount),
         });
     }
     return getApp();
 };
+
+export { getFirebaseAdminApp };
